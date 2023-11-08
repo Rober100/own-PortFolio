5f23dd45c2142e88df5101cb96479e4286cb165f
Rober <roberdz30@gmail.com>
2023-11-08 14:29:32 -0300

    Skills Component Done

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import "./skills.css"
+import Frontend from "./Fronted"
+import Backend from "./Backend"
 
 const Skills = () => {
   return (
     <section className="skills section" id="skills">
       <h2 className="section__title">Skills</h2>
       <span className="section__subtitle">Tecnologías</span>
+
+      <div className="skills__container container grid">
+        <Frontend />
+        <Backend />
+      </div>
     </section>
   );
 };
