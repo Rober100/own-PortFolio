5efcbf10896cd63a501b97c48f994b0cc2b7fa37
Rober <roberdz30@gmail.com>
2023-11-09 11:39:13 -0300

    Qualification Component finished, but Css is missing

--- src/components/qualification/Qualification.tsx
+++ src/components/qualification/Qualification.tsx
@@ -20,17 +20,116 @@ const Qualification = () => {
         </div>
 
         <div className="qualification__sections">
-            <div className="qualification__content">
-                <div className="qualification__data">
-                    <div>
-                        <h3 className="qualification__title"></h3>
-                        <span className="qualification__subtitle"> Henry - Argentina</span>
-                        <div className="qualification__calender">
-                            <i className="uil uil-calender-alt"></i> 2022 - Presente
-                        </div>
-                    </div>
+
+            {/* Aca están los Estudios */}
+          <div className="qualification__content qualification__content-active">
+            {/* Aca esta el primer titulo */}
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">
+                  Desarrollador Full Stack
+                </h3>
+                <span className="qualification__subtitle">
+                  {" "}
+                  Henry - Argentina
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calender-alt"></i> 2022 - Presente
                 </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
             </div>
+
+            {/* Aca esta el primer titulo */}
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">Otro título</h3>
+                <span className="qualification__subtitle">
+                  {" "}
+                  Otro Lugar - Francia
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calender-alt"></i> 2020 - 2023
+                </div>
+              </div>
+            </div>
+
+            {/* Aca vienen las experiencias */}
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">
+                  Desarrollador Full Stack Experiencia
+                </h3>
+                <span className="qualification__subtitle">
+                  {" "}
+                  Henry - Sudafrica
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calender-alt"></i> 2022 - año pasado
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+          </div>
+
+          {/* Aca están las experiencias */}
+          <div className="qualification__content">
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Aca poner experiencias</h3>
+                <span className="qualification__subtitle">
+                  Aca poner el lugar donde fue
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calender-alt"></i>Aca poner la fecha de
+                  la experiencia
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">Aca poner otra Experiencia</h3>
+                <span className="qualification__subtitle">
+                  Aca poner el lugar de la experiencia
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calender-alt"></i> fecha de la segunda experiencia
+                </div>
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
     </section>
