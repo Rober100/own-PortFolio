7de7e365a96430fe61757844000ac1f56c068a46
Rober <roberdz30@gmail.com>
2023-11-23 19:31:44 -0300

    Upp

--- src/components/work/Data.tsx
+++ src/components/work/Data.tsx
@@ -1,4 +1,4 @@
-import image1 from "../../assets/work1.jpg";
+import image1 from "../../assets/img2.png";
 import image2 from "../../assets/work2.jpg";
 import image3 from "../../assets/work3.jpg";
 import image4 from "../../assets/work4.jpg";
@@ -7,25 +7,28 @@ interface Project {
   id: number;
   title: string;
   description: string;
-  image: string;
+  image: Array;
   category: string;
+  link: string;
 }
 
 export const projects: Project[] = [
   {
     id: 1,
-    title: "Trendy Tech",
+    title: "Trendy Spot",
     description: "E-commerce de ventas de productos.",
-    image: image1,
+    image: [image1],
     category: "App",
+    link: "https://front-trendy-app.vercel.app/"
   },
   {
     id: 2,
-    title: "Henry Countries",
+    title: "Trendy Tech",
     description:
       "Henry Countries es una plataforma interactiva que te permite explorar el mundo y descubrir información detallada sobre países de una manera única y personalizada.",
     image: image2,
     category: "Web",
+    link: "https://front-trendy-app.vercel.app/"
   },
   {
     id: 3,
@@ -33,6 +36,7 @@ export const projects: Project[] = [
     description: "E-commerce de venta de ropa deportiva.",
     image: image3,
     category: "App",
+    link: "https://front-trendy-app.vercel.app/"
   },
   {
     id: 4,
@@ -40,6 +44,7 @@ export const projects: Project[] = [
     description: "Ta-te-tí",
     image: image4,
     category: "Design",
+    link: "https://front-trendy-app.vercel.app/"
   },
 ];
 
--- src/components/work/WorkItems.tsx
+++ src/components/work/WorkItems.tsx
@@ -3,7 +3,8 @@ interface WorkItemsProps {
     id: number;
     title: string;
     description: string;
-    image: string;
+    image: Array;
+    link: string;
   };
 }
 
@@ -12,7 +13,7 @@ const WorkItems: React.FC<WorkItemsProps> = ({ item }) => {
     <div className="work__card" key={item.id}>
       <img src={item.image} alt={item.title} className="work__img" />
       <h3 className="work__title">{item.title}</h3>
-      <a href="#" className="work__button">
+      <a href={item.link} className="work__button" target="_blank" rel="noopener noreferrer">
         Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
       </a>
     </div>
